5a53f1f3d5f5c3186413b4ad7f02e59ca51cd576
ecraela-czarina <avila.czarina10@gmail.com>
2024-02-04 23:28:22 +0800

    03 Assignment: Update Home Page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
     var lastModified = document.getElementById('lastModified');
     lastModified.textContent = "Last Modified: " + document.lastModified;
 });
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const map = document.querySelector('.map');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    map.classList.toggle('invisible'); // Toggle the 'invisible' class on the map element
+});
