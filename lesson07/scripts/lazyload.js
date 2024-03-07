e1ba3a201a4f8b3483e9170e0b0d1fe39430a621
ecraela-czarina <avila.czarina10@gmail.com>
2024-03-07 16:41:24 +0800

    lazyload

--- lesson07/scripts/lazyload.js
+++ lesson07/scripts/lazyload.js
@@ -1,28 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var lastModifiedElement = document.getElementById("lastModified");
-    var lastModified = new Date(document.lastModified);
-    lastModifiedElement.textContent = "Last modified: " + lastModified.toLocaleString();
 
-    var lazyImages = document.querySelectorAll('.lazy-image');
+var lastModifiedElement = document.getElementById("lastModified");
+var lastModified = new Date(document.lastModified);
+lastModifiedElement.textContent = "Last modified: " + lastModified.toLocaleString();
 
-    if ('IntersectionObserver' in window) {
-        var lazyImageObserver = new IntersectionObserver(function (entries, observer) {
-            entries.forEach(function (entry) {
-                if (entry.isIntersecting) {
-                    var lazyImage = entry.target;
-                    lazyImage.src = lazyImage.dataset.src;
-                    lazyImageObserver.unobserve(lazyImage);
-                }
-            });
-        });
+var lazyImages = document.querySelectorAll('.lazy-image');
 
-        lazyImages.forEach(function (lazyImage) {
-            lazyImageObserver.observe(lazyImage);
-        });
-    } else {
-        // Fallback for browsers that don't support IntersectionObserver
-        lazyImages.forEach(function (lazyImage) {
-            lazyImage.src = lazyImage.dataset.src;
-        });
-    }
-});
