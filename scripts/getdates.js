7c0bebbb457b10aef65e46bef44a0b1e12f47bd8
ecraela-czarina <avila.czarina10@gmail.com>
2024-03-07 23:18:34 +0800

    w07 homepage update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -17,3 +17,34 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
     map.classList.toggle('invisible');
 });
+
+
+// Number of Visits
+
+// Function to increment and update the visit count
+function incrementVisitCount() {
+    let visitCount = localStorage.getItem('visitCount');
+
+    visitCount = visitCount ? parseInt(visitCount) + 1 : 1;
+
+    localStorage.setItem('visitCount', visitCount);
+
+    document.getElementById('visit-count').textContent = visitCount;
+}
+
+
+function initializeVisitCount() {
+    let visitCount = localStorage.getItem('visitCount');
+
+    if (visitCount) {
+        document.getElementById('visit-count').textContent = visitCount;
+    } else {
+        localStorage.setItem('visitCount', 0);
+    }
+}
+
+// Call the functions
+initializeVisitCount();
+
+// Call the functions
+incrementVisitCount();
