8f7961ef5f4547669e7bd39c9fab59aa62925fac
ecraela-czarina <avila.czarina10@gmail.com>
2024-04-04 23:07:14 +0800

    Date on discover page

--- Demo/scripts/lazyload.js
+++
@@ -1,7 +0,0 @@
-
-var lastModifiedElement = document.getElementById("lastModified");
-var lastModified = new Date(document.lastModified);
-lastModifiedElement.textContent = "Last modified: " + lastModified.toLocaleString();
-
-// var lazyImages = document.querySelectorAll('.lazy-image');
-
--- lesson07/scripts/lazyload.js
+++ lesson07/scripts/lazyload.js
@@ -3,5 +3,5 @@ var lastModifiedElement = document.getElementById("lastModified");
 var lastModified = new Date(document.lastModified);
 lastModifiedElement.textContent = "Last modified: " + lastModified.toLocaleString();
 
-var lazyImages = document.querySelectorAll('.lazy-image');
+// var lazyImages = document.querySelectorAll('.lazy-image');
 
